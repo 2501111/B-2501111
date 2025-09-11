ec62b3fb495c511d78e5784d95341865fd6eeb38
2501111 <mauro00@kbu.ac.kr>
2025-09-11 13:59:45 +0900

    2주차

--- week02/Sample2/src/Main.java
+++ week02/Sample2/src/Main.java
@@ -8,7 +8,7 @@ public class Main { //메소드
 
         name = keyboard.nextLine();// Data 입력
 
-        // 처리
+        // 처리 ;
 
         // 출력 (필수) <- 필수니까 제일 1순위로 적기
         System.out.println("입력받은 이름 : " + name);
